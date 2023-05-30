53f2e78f7344ef888ad18e57f50901c7e83d8cae
Adam Dorling <adam.x.dorling@gmail.com>
2023-05-30 03:25:15 +0100

    chore: fix poetry blocks

--- src/parsers/fencedDiv.ts
+++ src/parsers/fencedDiv.ts
@@ -3,6 +3,13 @@ import type { ParserDef } from '../types'
 export const fencedDiv: ParserDef = {
   name: 'fencedDiv',
   regex: /^::: ?(?<cls>[^\n]+)?\n(?<content>[\s\S]+?)\n:::(?:\n|$)/,
-  handler: ({ cls, content }, { parse }) =>
+  handler: ({ cls, content }, { parse, parseIter }) =>
+    (console.log({
+      cls,
+      content,
+      l: `<div class="${['fenced', cls ?? ''].join(' ').trim()}">${parse(`\n${content}\n`)}</div>`,
+      e: '<div class="fenced info"><p>a</p><p>b</p><p>c</p><p>d</p></div>',
+      iter: [...parseIter(content)],
+    }) as unknown) ||
     `<div class="${['fenced', cls ?? ''].join(' ').trim()}">${parse(content)}</div>`,
 }
